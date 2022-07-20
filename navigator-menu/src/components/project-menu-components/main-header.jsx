107628fb27055829836b38b5eead6d27d8370eb7
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-07-20 21:58:34 +0300

    added current project name in the header

--- navigator-menu/src/components/project-menu-components/main-header.jsx
+++ navigator-menu/src/components/project-menu-components/main-header.jsx
@@ -1,10 +1,14 @@
 import {Outlet, NavLink} from 'react-router-dom';
 
 function MainHeader(){
+    let path = window.location.pathname;
+    let pathArrayName = path.split('/').filter(x => x);
+    let currentProject = pathArrayName.join(' ').toUpperCase();
 
     return(
         <>
             <NavLink to={'/'}>Back</NavLink>
+            <h1>{currentProject}</h1>
             <Outlet />
         </>
     )
