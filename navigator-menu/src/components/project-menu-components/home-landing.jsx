8b544097bc1209060ae95d446008d0d7ba04672d
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-07-20 22:34:43 +0300

    implemented a reusable route button component

--- navigator-menu/src/components/project-menu-components/home-landing.jsx
+++ navigator-menu/src/components/project-menu-components/home-landing.jsx
@@ -1,11 +1,7 @@
 import {NavLink} from 'react-router-dom';
 import '../../index.css';
 
-import GithubRouteBtn from './github-route-btn/github-route';
-import EComRouteBtn from './e-com-route-btn/e-com-route';
-import EqualizerRouteBtn from './equalizer-route-btn/equalizer-route';
-import LoopstudioRouteBtn from './loopstudio-route-btn/loopstudio-route';
-import TipsCalculatorRouteBtn from './tips-calculator-route-btn/tips-route';
+import RouteBtn from './routebtn-component';
 
 function Home(){
 
@@ -16,11 +12,15 @@ function Home(){
                 <h3 className='text-3xl text-white mt-[2rem]'>A navigational menu for all N2J Academy projects.</h3>
             </div>
             <div className="inline-flex flex-wrap gap-4 text-center w-4/5 mt-[6rem] mb-[4rem]">
-                <GithubRouteBtn />
-                <EComRouteBtn />
-                <EqualizerRouteBtn />
-                <LoopstudioRouteBtn />
-                <TipsCalculatorRouteBtn />
+                <RouteBtn navlinkStyling={"h-96 mx-1 p-8 rounded-xl bg-gray-50 text-2xl flex flex-col justify-between"} path={'project/tips-calculator'} imageStyling={'w-96'} imageSource={'./tipscalculator-thumbnail.jpg'}>Tips Calculator</RouteBtn>
+
+                <RouteBtn navlinkStyling={"h-96 mx-1 p-8 rounded-xl bg-gray-50 text-2xl flex flex-col justify-between"} path={'project/github'} imageStyling={'w-96'} imageSource={'./github-thumbnail.jpg'}>Github</RouteBtn>
+
+                <RouteBtn navlinkStyling={"h-96 mx-1 p-8 rounded-xl bg-gray-50 text-2xl flex flex-col justify-between"} path={'project/e-commerce'} imageStyling={'w-96'} imageSource={'./e-commerce-thumbnail.jpg'}>E-Commerce</RouteBtn>
+
+                <RouteBtn navlinkStyling={"h-96 mx-1 p-8 rounded-xl bg-gray-50 text-2xl flex flex-col justify-between"} path={'project/loop-studio'} imageStyling={'w-96'} imageSource={'./loopstudio-thumbnail.jpg'}>Loop-studio</RouteBtn>
+
+                <RouteBtn navlinkStyling={"h-96 mx-1 p-8 rounded-xl bg-gray-50 text-2xl flex flex-col justify-between"} path={'project/equalizer'} imageStyling={'w-96'} imageSource={'./equalizer-thumbnail.jpg'}>Equalizer</RouteBtn>
             </div>
         </>
     )
