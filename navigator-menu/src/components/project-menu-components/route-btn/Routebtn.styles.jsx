6657945a5530a55de5ccc09f3a5df4b6c1e1eef8
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-07-24 19:20:26 +0300

    added media query to the route btn component

--- navigator-menu/src/components/project-menu-components/route-btn/Routebtn.styles.jsx
+++ navigator-menu/src/components/project-menu-components/route-btn/Routebtn.styles.jsx
@@ -4,16 +4,28 @@ import {NavLink} from 'react-router-dom';
 export const StyledNavLink = tw(NavLink)`
     flex
     flex-col
-    justify-between    
-    h-[40rem]
-    w-[45rem]
+    justify-between
+    items-center    
+    h-[25rem]
+    w-[30rem]
     mx-1
     p-8
-    rounded-xl
-    bg-gray-50
+    rounded-sm
+    bg-mainMenuCardsBackground
     text-2xl
+    shadow-md
+
+    hover:bg-mainMenuCardsHoverBackground
+
+    phone:h-[18rem]
+
+    tablet:h-[25rem]
 `
 
 export const Image = tw.img`
-    w-full
+    w-10/12
+
+    hover:w-full
+
+    phone:w-full
 `
