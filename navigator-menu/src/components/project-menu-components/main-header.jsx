717508431142b3def08cc64382c19bf32f151dfa
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-10-01 16:16:10 +0300

    fixed the eslint config file, fixed tailwindcss imports, fixed prop-types

--- navigator-menu/src/components/project-menu-components/main-header.jsx
+++ navigator-menu/src/components/project-menu-components/main-header.jsx
@@ -1,24 +1,24 @@
-import {Outlet, NavLink} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { MdArrowBackIosNew } from 'react-icons/md';
-import {NavigationWrapper, StyledNavLink} from './Main-header.styles';
+import { NavigationWrapper, StyledNavLink } from './Main-header.styles';
 
-function MainHeader(){
-    let path = window.location.pathname;
-    let pathArrayName = path.split('/').filter(x => x);
-    let currentProject = pathArrayName.join(' ').toUpperCase();
+function MainHeader() {
+  let path = window.location.pathname;
+  let pathArrayName = path.split('/').filter((x) => x);
+  let currentProject = pathArrayName.join(' ').toUpperCase();
 
-    return(
-        <>
-            <NavigationWrapper>
-                <StyledNavLink to={'/'}>
-                    <MdArrowBackIosNew className='mr-[.5rem]'/>
-                    Back
-                </StyledNavLink>
-                <h1 className=''>{currentProject}</h1>
-            </NavigationWrapper>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <NavigationWrapper>
+        <StyledNavLink to={'/'}>
+          <MdArrowBackIosNew className="mr-[.5rem]" />
+          Back
+        </StyledNavLink>
+        <h1 className="">{currentProject}</h1>
+      </NavigationWrapper>
+      <Outlet />
+    </>
+  );
 }
 
 export default MainHeader;
--- navigator-menu/src/components/project-menu-components/route-btn/Routebtn-Component.jsx
+++ navigator-menu/src/components/project-menu-components/route-btn/Routebtn-Component.jsx
@@ -1,13 +1,19 @@
-import {StyledNavLink, Image} from './Routebtn.styles';
+import { StyledNavLink, Image } from './Routebtn.styles';
+import PropTypes from 'prop-types';
 
-function RouteBtn(props){
-
-    return(
-        <StyledNavLink to={props.path}>
-            <Image src={props.imageSource}/>
-            {props.children}
-        </StyledNavLink>
-    )
+function RouteBtn(props) {
+  return (
+    <StyledNavLink to={props.path}>
+      <Image src={props.imageSource} />
+      {props.children}
+    </StyledNavLink>
+  );
 }
+
+RouteBtn.propTypes = {
+  path: PropTypes.string,
+  imageSource: PropTypes.string,
+  children: PropTypes.string,
+};
 
 export default RouteBtn;
